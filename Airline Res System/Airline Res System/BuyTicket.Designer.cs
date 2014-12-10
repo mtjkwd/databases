c7e8e039ccbafaccd1f644d1d7f00c040a8b45b0
Michael Jolley <mtjkwd@mst.edu>
2014-12-09 23:50:36 -0600

    GUI

--- Airline Res System/Airline Res System/BuyTicket.Designer.cs
+++ Airline Res System/Airline Res System/BuyTicket.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(344, 177);
+            this.ClientSize = new System.Drawing.Size(513, 306);
             this.Name = "BuyTicket";
             this.Text = "BuyTicket";
             this.ResumeLayout(false);
